6736a2421488a0e5f695608d63b7ee2a74f5c7f7
farzin89 <farzin_ahmadi90@yahoo.com>
2022-04-22 19:25:08 +0200

    face detection with harcascade

---
+++ 2. face detection with haarcascade algorithm/face detection_harcascade.py
@@ -0,0 +1,29 @@
+import numpy as np
+import cv2
+
+face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_alt2.xml")
+
+
+
+cap = cv2.VideoCapture(0)
+
+while True :
+    # capture frame by frame
+    ret,frame = cap.read()
+    gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray,scaleFactor=1.5,minNeighbors=5)
+    for (x,y,w,h) in faces :
+        print(x,y,w,h)
+        roi_gray = gray[y:y+h,x:x+w]
+        roi_color = frame[y:y+h,x:x+w]
+
+        img_item = "my-image.png"
+        cv2.imwrite(img_item,roi_gray)
+        cv2.imwrite("image_color.png",roi_color)
+    # display the resulting frame
+    cv2.imshow('frame',frame)
+    if cv2.waitKey(20) & 0xFF == ord('q') :
+        break
+# when everything done,release the capture
+cap.release()
+cv2.destroyAllWindows()
