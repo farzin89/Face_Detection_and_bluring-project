f7337680fb830cca2f6a823bf9bf71eec766b4ea
farzin89 <farzin_ahmadi90@yahoo.com>
2022-04-22 20:07:59 +0200

    rectangular on detected faces

--- 2. face detection with haarcascade algorithm/face detection_harcascade.py
+++ 2. face detection with haarcascade algorithm/face detection_harcascade.py
@@ -14,12 +14,18 @@ while True :
     faces = face_cascade.detectMultiScale(gray,scaleFactor=1.5,minNeighbors=5)
     for (x,y,w,h) in faces :
         print(x,y,w,h)
-        roi_gray = gray[y:y+h,x:x+w]
+        roi_gray = gray[y:y+h,x:x+w] # (y cord_start,ycord_end) and also (x cord start, x cord end)
         roi_color = frame[y:y+h,x:x+w]
 
         img_item = "my-image.png"
         cv2.imwrite(img_item,roi_gray)
         cv2.imwrite("image_color.png",roi_color)
+
+        color = (255,0,0)
+        stroke = 2
+        end_cord_x = x + w
+        end_cord_y = y + h
+        cv2.rectangle(frame,(x,y),(end_cord_x ,end_cord_y),color,stroke) #end_cord_x = width ,end_cord_y = height
     # display the resulting frame
     cv2.imshow('frame',frame)
     if cv2.waitKey(20) & 0xFF == ord('q') :
