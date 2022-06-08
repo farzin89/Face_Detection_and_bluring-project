344a87ce37555ee4fbc1e01932ed3821770acac4
farzin89 <farzin_ahmadi90@yahoo.com>
2022-06-08 17:55:36 +0200

    second project with haarcascade algorithm

---
+++ 7. face detection with haarcascade 2/face detection with haarcascade 2.py
@@ -0,0 +1,9 @@
+
+import os
+import cv2
+from time import time
+#import mediapipe as mp
+import matplotlib as plt
+
+cascade_face_detector = cv2.CascadeClassifier("haarcascade_frontalface_alt2.xml")
+
